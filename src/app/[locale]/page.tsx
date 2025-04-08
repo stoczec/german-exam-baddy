7dc4a287fbe8b74055110344966919416b9a6d81
Dmytro Herashchenko <stoczec@gmail.com>
2025-04-08 05:44:32 +0200

    `feat: added isLoading state and setIsLoading function to store, updated Home component to display Skeleton component while isLoading is true`

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { LanguageSelect, ModeToggle, NavMenu } from "@/components";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
@@ -13,19 +15,29 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useStore } from "@/store";
 import { SelectTrigger } from "@radix-ui/react-select";
 import { useTranslations } from "next-intl";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { isLoading, setIsLoading } = useStore();
   const t = useTranslations("buttons");
+
+  useEffect(() => {
+    setIsLoading(false);
+    return () => {
+      setIsLoading(true);
+    };
+  }, [setIsLoading]);
+
   return (
     <div className="flex flex-col items-center justify-center gap-4 h-screen">
       <div>
         <NavMenu />
       </div>
-      <div>
-        <LanguageSelect />
-      </div>
+      <div>{!isLoading ? <LanguageSelect /> : <Skeleton className="w-[180px] h-[36px]" />}</div>
       <div>
         <ModeToggle />
       </div>
@@ -35,12 +47,16 @@ export default function Home() {
       </div>
       <Separator />
       <div className="flex gap-2">
-        <Avatar id="avatar">
-          <AvatarImage
-            src="https://imgs.search.brave.com/8ylarQ0Xf9DIrDR0ypzj4DPdPWmPmoUhgkhi4SwM8gw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTE3/OTk4MjY0L3ZlY3Rv/ci9tYWxlLXVzZXIt/aWNvbi5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9NFJNaHFJ/WGNKTWNGa1JKUHE2/SzhoN296dVVvWmhQ/d0tuaUVrZTZLWWFf/az0"
-            alt="avatar"
-          />
-        </Avatar>
+        {!isLoading ? (
+          <Avatar id="avatar">
+            <AvatarImage
+              src="https://imgs.search.brave.com/8ylarQ0Xf9DIrDR0ypzj4DPdPWmPmoUhgkhi4SwM8gw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTE3/OTk4MjY0L3ZlY3Rv/ci9tYWxlLXVzZXIt/aWNvbi5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9NFJNaHFJ/WGNKTWNGa1JKUHE2/SzhoN296dVVvWmhQ/d0tuaUVrZTZLWWFf/az0"
+              alt="avatar"
+            />
+          </Avatar>
+        ) : (
+          <Skeleton className="w-[32px] h-[32px] rounded-full" />
+        )}
         <Label htmlFor="avatar">Avatar</Label>
       </div>
       <Separator />
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,11 @@
 import { create } from "zustand";
 
-interface IUseStore {}
+interface IUseStore {
+  isLoading: boolean;
+  setIsLoading: (isLoading: boolean) => void;
+}
 
-export const useStore = create<IUseStore>()((set) => ({}));
+export const useStore = create<IUseStore>()((set) => ({
+  isLoading: true,
+  setIsLoading: (isLoading: boolean) => set({ isLoading }),
+}));
